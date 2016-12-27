461db29e2897c786f7650d3730b98efd38725daf
Nicolas Berrizbeitia <nbr625@gmail.com>
2016-12-27 02:58:43 -0800

    Create and delete flash messages

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import NavigationBar from './NavigationBar.js'
+import NavigationBar from './NavigationBar.js';
+import FlashMessagesList from './flash/FlashMessagesList';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <NavigationBar />
+        <FlashMessagesList />
         {this.props.children}
       </div>
     );
