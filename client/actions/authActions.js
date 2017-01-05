5e7f066e367a6124c72fdabd7e8e7085d502dd11
Nicolas Berrizbeitia <nbr625@gmail.com>
2017-01-05 04:11:59 -0800

    login and logout on client

--- client/actions/authActions.js
+++ client/actions/authActions.js
@@ -10,10 +10,19 @@ export function setCurrentUser(user){
   };
 }
 
+export function logout() {
+  return dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthorizationToken(false);
+    dispatch(setCurrentUser({}));
+  }
+}
+
 export function login (data) {
   return dispatch => {
     return axios.post('/api/auth', data).then(res => {
       const token = res.data.token;
+
       localStorage.setItem('jwtToken', token);
       setAuthorizationToken(token);
       dispatch(setCurrentUser(jwt.decode(token)));
